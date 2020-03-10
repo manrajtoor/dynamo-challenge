69ac8966d974c94d2ed51cceaf7d37a1d320becf
Manraj <manrajt@umich.edu>
2020-03-10 15:20:28 -0400

    Functional API fetching

--- js/table.jsx
+++ js/table.jsx
@@ -5,9 +5,20 @@ class Table extends React.Component {
 
   // }
 
-  // componentDidMount() {
-
-  // }
+  componentDidMount() {
+    const fetch_url = "http://dummy.restapiexample.com/api/v1/employees";
+    // Call REST API to get employees
+    fetch(fetch_url)
+    // If bad response from url
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => { console.log(error); });
+  }
   render() {
     return (
       <div>
