ca6e7d19f71028763f4dbe08b39de9c57e4f1a22
Manraj <manrajt@umich.edu>
2020-03-10 16:23:20 -0400

    Basic table implemented

--- js/table.jsx
+++ js/table.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 class Table extends React.Component {
-  // constructor() {
-
-  // }
+  constructor(props) {
+    super(props)
+    this.state = { employees: [] };
+    this.renderRows = this.renderRows.bind(this);
+  }
 
   componentDidMount() {
     const fetch_url = "http://dummy.restapiexample.com/api/v1/employees";
@@ -15,14 +17,44 @@ class Table extends React.Component {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
+        if (data.status != 'success') throw Error('Failed API Fetch');
+        else {
+          this.setState({
+            employees: data.data,
+          });
+        }
       })
       .catch((error) => { console.log(error); });
   }
+
+  renderRows() {
+    return this.state.employees.map((item, key) => {
+      const {id, employee_name, employee_salary, employee_age, profile_image} = item;
+      console.log(item)
+      console.log(name)
+
+      return (
+        <tr key={id}>  
+          <td>{id}</td>
+          <td>{employee_name}</td>
+          <td>{employee_salary}</td>
+          <td>{employee_age}</td>
+          <td>{profile_image}</td>
+        </tr>
+      )
+    }
+    );
+  }
+
   render() {
     return (
       <div>
-      <p>Hello World!</p>
+        <table>
+          <tbody>
+            {this.renderRows()}
+          </tbody>
+        </table>
+        <p>Hello World!</p>
       </div>
     );
   }
